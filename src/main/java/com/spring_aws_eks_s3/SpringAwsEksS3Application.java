71d2324caff1fa83675e483ab609f7c82f986319
manivannancsmca <manivannancsmca@gmail.com>
2026-06-22 18:36:24 +0530

    first commit

---
+++ src/main/java/com/spring_aws_eks_s3/SpringAwsEksS3Application.java
@@ -0,0 +1,13 @@
+package com.spring_aws_eks_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsEksS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsEksS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_aws_eks_s3/SpringAwsEksS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_aws_eks_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsEksS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
